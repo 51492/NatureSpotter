f096013ddc2bd07410127844a0397de82b788524
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-11-12 11:23:58 +0000

    いいねボタン非同期化

--- app/controllers/users/likes_controller.rb
+++ app/controllers/users/likes_controller.rb
@@ -1,19 +1,19 @@
 class Users::LikesController < ApplicationController
-  
+
   def create
-    post = Post.find(params[:post_id])
-    like = current_user.likes.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.new(post_id: @post.id)
     like.save
-    redirect_to request.referer
+    # redirect_to request.referer ←redirect先を消すことでコントローラー側がjsファイルを探す
   end
-  
+
   def destroy
-    post = Post.find(params[:post_id])
-    like = current_user.likes.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.find_by(post_id: @post.id)
     like.destroy
-    redirect_to request.referer
+    # redirect_to request.referer ←redirect先を消すことでコントローラー側がjsファイルを探す
   end
-  
+
 end
 
 # 以下参考
