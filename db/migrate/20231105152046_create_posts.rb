5b72650511b8436b75c28e2016c2d50e20d0b9b1
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-11-25 10:01:16 +0000

    [update] migration float limit

--- db/migrate/20231105152046_create_posts.rb
+++ db/migrate/20231105152046_create_posts.rb
@@ -5,8 +5,8 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.text :caption
       t.string :place, null: false
       t.string :address, null: false
-      t.float :post_latitude
-      t.float :post_longitude
+      t.float :post_latitude, :limit=>53
+      t.float :post_longitude, :limit=>53
 
       t.timestamps
     end
