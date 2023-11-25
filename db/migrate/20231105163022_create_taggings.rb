160a70810caa8bb338cf88d747f95e90798a4487
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-11-25 09:22:11 +0000

    [update]migration

--- db/migrate/20231105163022_create_taggings.rb
+++ db/migrate/20231105163022_create_taggings.rb
@@ -1,8 +1,8 @@
 class CreateTaggings < ActiveRecord::Migration[6.1]
   def change
     create_table :taggings do |t|
-      t.references :post, null: false, foreign_key: { to_table: :posts } # post_idの外部キー制約を設定
-      t.references :tag, null: false, foreign_key: { to_table: :tags } # tag_idの外部キー制約を設定
+      t.references :post, null: false,type: :bigint, foreign_key: { to_table: :posts } # post_idの外部キー制約を設定
+      t.references :tag, null: false,type: :bigint, foreign_key: { to_table: :tags } # tag_idの外部キー制約を設定
 
       t.timestamps
     end
